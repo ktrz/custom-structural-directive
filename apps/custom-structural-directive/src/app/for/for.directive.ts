d58c5b5c06339cb80a106034d870f71c1c9211df
Chris Trzesniewski <k.trzesniewski@gmail.com>
2021-11-05 14:53:49 +0100

    feat: add csdFor context params

--- apps/custom-structural-directive/src/app/for/for.directive.ts
+++ apps/custom-structural-directive/src/app/for/for.directive.ts
@@ -18,8 +18,16 @@ export class ForDirective<T> {
 
   private renderItems(): void {
     this.vcr.clear();
-    this.items.map(() => {
-      this.vcr.createEmbeddedView(this.templateRef);
+    this.items.map((item, index, arr) => {
+      this.vcr.createEmbeddedView(this.templateRef, {
+        $implicit: item,
+        index,
+        first: index === 0,
+        last: index === arr.length - 1,
+        even: (index & 1) === 0,
+        odd: (index & 1) === 1,
+        count: arr.length
+      });
     });
   }
 }
